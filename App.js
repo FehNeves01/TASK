a7f8358e7717026f2adf0f2f8395561584f90b58
FELIPE BARBOSA NEVES <felipe.neves01@gmail.com>
2021-12-08 11:21:17 -0300

    configurado notification push

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import NewTask  from "./src/pages/NewTask/"
 import Details  from "./src/pages/Details/"
 import Login    from "./src/pages/Login/"
 import NewUser  from "./src/pages/NewUser/"
+import TesteApp from './src/pages/TestePage';
+
 
 const Stack = createStackNavigator();
 
@@ -15,11 +17,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Task"     component={Task}    options={{ headerTintColor: "#172051", headerLeft:null}} />
-        <Stack.Screen name="NewTask"  component={NewTask} options={{ headerTintColor: "#172051" }} />
-        <Stack.Screen name="Details"  component={Details} options={{ headerTintColor: "#172051" }} />
-        <Stack.Screen name="Login"    component={Login}   options={{ headerShown:false}} />
-        <Stack.Screen name="NewUser"  component={NewUser} options={{ headerShown:false}} />
+        <Stack.Screen name="Task"       component={Task}      options={{ headerTintColor: "#172051", headerLeft:null}} />
+        <Stack.Screen name="NewTask"    component={NewTask}   options={{ headerTintColor: "#172051" }} />
+        <Stack.Screen name="Details"    component={Details}   options={{ headerTintColor: "#172051" }} />
+        <Stack.Screen name="Login"      component={Login}     options={{ headerShown:false}} />
+        <Stack.Screen name="NewUser"    component={NewUser}   options={{ headerShown:false}} />
+        <Stack.Screen name="TesteApp"   component={TesteApp}  options={{ headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/pages/TestePage/index.js
+++ src/pages/TestePage/index.js
@@ -1,114 +1,189 @@
-import React, { useState, useEffect } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, StyleSheet } from 'react-native';
 
-const TesteApp = () => {
-
-  function prepararJson() {
-    return {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "email": "alunodeveloper@developer.com",
-        "password": "172051"
-      })
-    };
-  }
-
-  useEffect(() => {
-    (async () => {
-
-
-
-      // console.log(await Axios.post("https://192.168.9.157/api/login", {
-      //   params: {
-      //     "email": "alunodeveloper@developer.com",
-      //     "password": "172051"
-      //   }
-      // }))
-
-
-      var isAutenticado = false;
-      await fetch("http://192.168.9.154/api/login", prepararJson())
-        .then((Response) => (Response.json()))
-        .then((resJson) => {
-          var status = (resJson.status);
-          if (status === 401) {
-            isAutenticado = false;
-          } else if (resJson.token != "") {
-            isAutenticado = true;
-            dispatch({
-              type: LOGIN_VALUE,
-              payload: resJson,
-            })
-          } else {
-            imprimirAlert("Error, favor entrar em Contato");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          return error;
-        });
-      console.log(isAutenticado);
-
-    })();
-
-
-  }, []);
-  return (
-    <View style={styles.centeredView}>
-      <Text>Ola Neo</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-
-  },
-  modalView: {
-    width: "80%",
-    height: "80%",
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
-export default TesteApp;
+export default function TesteApp({navigation}) {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+      console.log('bye bye');
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
+      <Button
+        title="tela Login"
+        onPress={() => {
+           mudarDeTela(navigation);
+        }}
+      />
+    </View>
+  );
+}
+function mudarDeTela(navigation){
+  navigation.navigate('Login');
+}
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log('asdasd    |  |  |', token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+
+
+
+// import React, {useState, useEffect} from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+
+
+// export default function TesteApp({ }) {
+//   useEffect(() => {
+//      console.log();
+//   })
+//   return (
+//      <View style={styles.centeredView}>
+//        <Text>Olá Neo</Text>
+//     </View>
+//   )
+//  }
+
+
+// const styles = StyleSheet.create({
+//   centeredView: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     marginTop: 22,
+
+//   },
+//   modalView: {
+//     width: "80%",
+//     height: "80%",
+//     margin: 20,
+//     backgroundColor: "white",
+//     borderRadius: 20,
+//     padding: 35,
+//     alignItems: "center",
+
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 2
+//     },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 4,
+//     elevation: 5
+//   },
+//   button: {
+//     borderRadius: 20,
+//     padding: 10,
+//     elevation: 2
+//   },
+//   buttonOpen: {
+//     backgroundColor: "#F194FF",
+//   },
+//   buttonClose: {
+//     backgroundColor: "#2196F3",
+//   },
+//   textStyle: {
+//     color: "white",
+//     fontWeight: "bold",
+//     textAlign: "center"
+//   },
+//   modalText: {
+//     marginBottom: 15,
+//     textAlign: "center"
+//   }
+// });
